6dec9a4e3815f91d71a0048c54c3f4fda8846161
takinbiyi127001 <51786708+takinbiyi127001@users.noreply.github.com>
2022-05-06 07:12:34 +1000

    updated

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,8 +4,7 @@ from app.calc import add
 
 class CalcTest(TestCase):
 
-    def test_add_numbers(self):
-        """Test that two numbers are added together"""
-        self.assertEqual(add(3, 8), 11)
 
-#docker-compose run app sh -c "python manage.py test"
+    def test_add_numbers(self):
+
+        self.assertEqual(add(3, 8), 11)
